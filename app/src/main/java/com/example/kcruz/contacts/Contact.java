114c450ef8d9e004a694686cd01f7b8df587988e
00010216 <00010216@uca.edu.sv>
2018-04-29 14:22:51 -0600

    modificado clase solo para firstName, lastName,number y image

--- app/src/main/java/com/example/kcruz/contacts/Contact.java
+++ app/src/main/java/com/example/kcruz/contacts/Contact.java
@@ -8,12 +8,52 @@ public class Contact implements Parcelable {
     private String lastName;
     private String number;
     private int image;
-    private String birthDate;
-    private String email;
+    //private String birthDate;
+    //private String email;
 
-    public Contact(){
+    public Contact(String firstName, String lastName, String number, int image){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+        this.image = image;
 
     }
+
+    protected Contact(Parcel in) {
+        firstName = in.readString();
+        lastName = in.readString();
+        number = in.readString();
+        image = in.readInt();
+        //birthDate = in.readString();
+       // email = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+        dest.writeString(number);
+        dest.writeInt(image);
+        /*dest.writeString(birthDate);
+        dest.writeString(email);*/
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel in) {
+            return new Contact(in);
+        }
+
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
 
 
     public String getFirstName() {
@@ -48,7 +88,7 @@ public class Contact implements Parcelable {
         this.image = image;
     }
 
-    public String getBirthDate() {
+   /* public String getBirthDate() {
         return birthDate;
     }
 
@@ -62,42 +102,7 @@ public class Contact implements Parcelable {
 
     public void setEmail(String email) {
         this.email = email;
-    }
+    }*/
 
 
-    protected Contact(Parcel in) {
-        firstName = in.readString();
-        lastName = in.readString();
-        number = in.readString();
-        image = in.readInt();
-        birthDate = in.readString();
-        email = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(firstName);
-        dest.writeString(lastName);
-        dest.writeString(number);
-        dest.writeInt(image);
-        dest.writeString(birthDate);
-        dest.writeString(email);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
-        @Override
-        public Contact createFromParcel(Parcel in) {
-            return new Contact(in);
-        }
-
-        @Override
-        public Contact[] newArray(int size) {
-            return new Contact[size];
-        }
-    };
 }
