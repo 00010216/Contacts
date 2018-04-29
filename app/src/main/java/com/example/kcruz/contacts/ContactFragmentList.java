1233b9c8768d9b7b066d988fe3eafe0c1ad9c645
00010216 <00010216@uca.edu.sv>
2018-04-29 14:20:03 -0600

    agregado string-array de prueba

--- app/src/main/java/com/example/kcruz/contacts/ContactFragmentList.java
+++ app/src/main/java/com/example/kcruz/contacts/ContactFragmentList.java
@@ -5,12 +5,14 @@ import android.app.FragmentTransaction;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContactList extends ListFragment implements AdapterView.OnItemClickListener {
@@ -24,31 +26,36 @@ public class ContactList extends ListFragment implements AdapterView.OnItemClick
         return view;
     }
 
-
+//Debe recibir valores del celular y de los ingresados por usuario
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.firstName, android.R.layout.simple_list_item_1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                R.array.firstName, android.R.layout.simple_list_item_1) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(Color.WHITE);
+                return view;
+            }
+        };
         setListAdapter(adapter);
         getListView().setOnItemClickListener(this);
     }
 
-
-
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
         //se envia a objeto el contenido en la posicion i segun click
-        //contact = new Contact(
-                //getResources().getStringArray(R.array.firstName)[i]);/*
-                /*getResources().obtainTypedArray(R.array.ImgId).getResourceId(i,-1),
-                getResources().getStringArray(R.array.Description)[i]);*/
+        contact = new Contact(getResources().getStringArray(R.array.firstName)[i],
+                getResources().getStringArray(R.array.lastName)[i],getResources().getStringArray(R.array.number)[i],
+                getResources().obtainTypedArray(R.array.image).getResourceId(i,-1));
 
-      /*  Bundle bundle = new Bundle(); //procesa la info que se enviara a traves del intent
-        bundle.putParcelable("KEY", planetInfo); //manda identificador de bundle
+        Bundle bundle = new Bundle(); //procesa la info que se enviara a traves del intent
+        bundle.putParcelable("KEY", contact); //manda identificador de bundle
 
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
-            Intent newIntent = new Intent(getActivity().getApplicationContext(), Main2Activity.class);
+            Intent newIntent = new Intent(getActivity().getApplicationContext(), ContactInformationActivity.class);
             newIntent.setAction(Intent.ACTION_SEND);
             newIntent.putExtras(bundle);
             startActivity(newIntent);
