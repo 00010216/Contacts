9711f7bd1728c1109973609532e9ef0a7e274baf
00010216 <00010216@uca.edu.sv>
2018-05-01 15:39:23 -0600

    se agrego para que el ToolBar actue como ActionBar para esta actividad

--- app/src/main/java/com/example/kcruz/contacts/ContactAdditionActivity.java
+++ app/src/main/java/com/example/kcruz/contacts/ContactAdditionActivity.java
@@ -2,6 +2,7 @@ package com.example.kcruz.contacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class ContactAdditionActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class ContactAdditionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_addition);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //hacer que el ToolBar actue como el ActionBar para esta actividad
     }
 }
