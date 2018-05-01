bd68f194a55c1039ec07a5e3b80fb33ed168d5ec
00010216 <00010216@uca.edu.sv>
2018-05-01 15:35:35 -0600

    se agrego funcionalidad de imagen para agregar contacto y abrir la actividad ContactAdditionActivity

--- app/src/main/java/com/example/kcruz/contacts/MainActivity.java
+++ app/src/main/java/com/example/kcruz/contacts/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.kcruz.contacts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView addContactIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addContactIcon = findViewById(R.id.img_add_contact);
+        addContactIcon.setOnClickListener(add);
+    }
+
+    View.OnClickListener add = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openAddContact(v); //dandole funcionalidad a imagen para abrir activity ContactAddition
+        }
+    };
+
+
+    public void openAddContact(View view) {
+        //como segundo parametro se indica la actividad a la que ira este intent
+        Intent intent = new Intent(this, ContactAdditionActivity.class);
+        startActivity(intent); //iniciando la otra actividad
     }
 }
