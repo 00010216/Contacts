b525d24e9fc284eb43d72e550b59c4c884458fb1
00010216 <00010216@uca.edu.sv>
2018-05-04 22:01:29 -0600

    redefinicion de la clase contact se le agrego id y metodos hasCode y equals

--- app/src/main/java/com/example/kcruz/contacts/beans/Contact.java
+++ app/src/main/java/com/example/kcruz/contacts/beans/Contact.java
@@ -1,4 +1,82 @@
 package com.example.kcruz.contacts.beans;
 
 public class Contact {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String number;
+
+    public Contact() {
+        super();
+    }
+
+    public Contact(int id, String firstName, String lastName, String number) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.number = number;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(obj == null)
+            return false;
+        if(getClass() != obj.getClass())
+            return false;
+        Contact other = (Contact) obj;
+        if(id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact = [id="+id + ", name=" + firstName + ", last name = " + lastName + ", number=" +number;
+    }
+
+    public String getContactName(){
+        return firstName + " " + lastName;
+    }
 }
